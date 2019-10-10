85d7dd05be0523b8d513d319053437431770a904
William Entriken <github.com@phor.net>
2019-10-09 21:51:13 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,3 +3,24 @@ import PackageDescription
 let package = Package(
   name: "FDSoundActivatedRecorder"
 )
+
+let package = Package(
+    name: "FDSoundActivatedRecorder"
+    platforms: [.iOS(.v10)],
+    products: [
+        .library(name: "FDSoundActivatedRecorder", targets: ["FDSoundActivatedRecorder"])
+    ],
+    targets: [
+        .target(
+            name: "FDSoundActivatedRecorder",
+            path: "Sources",
+        //    exclude: ["DiskExample"]
+        ),
+        .testTarget(
+            name: "FDSoundActivatedRecorderTests",
+            dependencies: ["FDSoundActivatedRecorder"],
+            path: "Tests",
+        //    exclude:  ["DiskExample"]
+        )
+    ]
+)
